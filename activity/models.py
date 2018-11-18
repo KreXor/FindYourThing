cce1cc38e5b90bc598b31e17723454d5171cdf69
Daniel Johansson <daniel.to.johansson@gmail.com>
2018-11-18 14:37:27 +0000

    Added autocomplet for city search. Implemented ajax call for search. Added url hash to be able to remeber last search term. Remove buttons on activity details

--- activity/models.py
+++ activity/models.py
@@ -6,7 +6,7 @@ class ActivityType(models.Model):
     name = models.CharField(max_length=30)
 
     def __str__(self):
-        return self.name
+        return self.name	
 
 class Activity(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
@@ -15,9 +15,12 @@ class Activity(models.Model):
     type = models.ForeignKey(ActivityType, on_delete=models.CASCADE)
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
-    webpage = models.URLField(max_length=200)
-    instagram = models.URLField(max_length=200)
-    facebook = models.URLField(max_length=200)
+    webpage = models.URLField(max_length=200, blank=True, null=True)
+    instagram = models.URLField(max_length=200, blank=True, null=True)
+    facebook = models.URLField(max_length=200, blank=True, null=True)
+    long = models.FloatField(blank=True, null=True)
+    lat = models.FloatField(blank=True, null=True)
+    address = models.CharField(max_length=200, null=True)
 
     def publish(self):
         self.published_date = timezone.now()
--- activity/views.py
+++ activity/views.py
@@ -1,10 +1,33 @@
+import json
+
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Activity
 from django.http import HttpResponse
+from django.core import serializers
 
+from django.views.decorators.csrf import ensure_csrf_cookie
+
+@ensure_csrf_cookie
 def activity_list(request):
-    activities = Activity.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    if request.is_ajax():
+        location = request.POST.get('location')
+        if(location == ''):
+            return HttpResponse(json.dumps(''),
+			    content_type='application/json')
+        location = location.split(',')
+        activities = Activity.objects.filter(address__contains=location[0]).order_by('published_date')
+        # do whatever processing you need
+        # user.some_property = whatever
+
+        # send back whatever properties you have updated
+        #json_response = {'activities': activities}
+        json_response = serializers.serialize("json", activities.all())
+        #json_response = {'location': location}
+        return HttpResponse(json.dumps(json_response),
+            content_type='application/json')
+	
+    activities = Activity.objects.order_by('published_date')
     return render(request, 'activity/activity_list.html', {'activities': activities})
 	
 def activity_detail(request, activity_id):
